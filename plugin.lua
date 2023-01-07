2d79f4f10b8fc3f56cfeda1986753d7ee69dafee
Aoldri <34401249+Aoldri@users.noreply.github.com>
2023-01-07 15:00:55 +0000

    Improve class plugin
    Fixed weird duplication of class when defining consecutive classes

--- plugin.lua
+++ plugin.lua
@@ -6,12 +6,12 @@ function OnSetText(uri, text)
 
     -- Subclass
     for start, new_class, super_class, finish 
-    in text:gmatch '()class "([%a%d%u_]+)"[ ]?%(?([%w_.]*)%)?()' do
+    in text:gmatch '()class "([%w_]+)"[ ]?%(?([%w_%.]*)%)?()' do
         if super_class ~= "" then super_class = ":"..super_class end
         diffs[#diffs+1] = {
-            start  = start,
-            finish = start-1,
-            text   = ('---@class %s%s\n---@overload fun(): %s\n%s = {}\n'):format(new_class, super_class, new_class, new_class),
+            start  = finish+1,
+            finish = finish,
+            text   = ('\n---@class %s%s\n---@overload fun(): %s\n%s = {}'):format(new_class, super_class, new_class, new_class),
         }
         diffs[#diffs+1] = {
             start = start,
