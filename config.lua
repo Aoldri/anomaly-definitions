143a8f97358185bc30c4691f096996066422904d
Aoldri <34401249+Aoldri@users.noreply.github.com>
2023-02-06 23:35:50 +0000

    feat: add recommended config settings

--- config.lua
+++
@@ -1,5 +0,0 @@
--- A list of filenames to look for in the workspace. If a
--- match is found, this environment will be recommended
-files = {
-    "scripts/.*%.script" -- wildcard match any .script file in scripts/
-}
